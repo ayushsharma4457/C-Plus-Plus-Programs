1b8b30976627b0b273ac50c5deb4f99c0297d157
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-13 17:59:46 +0530

    Data Types
    Data Types in C++ language.

---
+++ 01_dataTypes/01_dataTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 54;
+    float b = 4.54;
+    char c = 'a';
+    double d = 5.23654;
+    bool e = true;
+
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+
+    return 0;
+}
