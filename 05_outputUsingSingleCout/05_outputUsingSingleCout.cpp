41bc0113bf35901154c866a5f8554fd1b287703f
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-14 19:15:18 +0530

    Output Using Single Cout
    Output Using Single Cout

---
+++ 05_outputUsingSingleCout/05_outputUsingSingleCout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maths = 90, physics = 77, chemistry = 69;
+
+    cout << "Maths = " << maths << "\nPhysics = " << physics << "\nChemistry = " << chemistry;
+
+    return 0;
+}
