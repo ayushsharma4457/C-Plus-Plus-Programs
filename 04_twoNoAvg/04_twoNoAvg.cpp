2d81fe5cf1ff84680aeee4e00b37a67715252b2c
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-14 17:53:49 +0530

    Average Of Two Numbers
    Average Of Two Numbers

---
+++ 04_twoNoAvg/04_twoNoAvg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "Enter The Number 1: ";
+    cin >> a;
+    cout << "Enter The Number 2: ";
+    cin >> b;
+
+    cout << "Average Of Two Numbers: " << (a + b) / 2;
+
+    return 0;
+}
