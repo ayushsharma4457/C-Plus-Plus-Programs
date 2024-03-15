e796d48df313b8b73b4e9845efc37b2241ff8a0a
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-15 17:22:01 +0530

    DataTypes

--- 01_dataTypes/01_dataTypes.cpp
+++ 01_dataTypes/01_dataTypes.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -8,12 +8,15 @@ int main()
     char c = 'a';
     double d = 5.23654;
     bool e = true;
+    wchar_t f = L'A';
 
-    cout<<a<<endl;
-    cout<<b<<endl;
-    cout<<c<<endl;
-    cout<<d<<endl;
-    cout<<e<<endl;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+    cout << f << endl;
+    cout << sizeof(f);
 
     return 0;
 }
