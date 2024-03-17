0e572a08aebd32ae63cc01d89755b9d162c18e2f
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-17 13:28:05 +0530

    Char To ACSII
    read char and print ACSII value.

---
+++ 10_ASCII_valueOfChar/10_ASCII_valueOfChar.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char character;
+
+    cout << "Enter The Char: ";
+    cin >> character;
+
+    cout << "ASCII Value: " << int(character);
+
+    return 0;
+}
