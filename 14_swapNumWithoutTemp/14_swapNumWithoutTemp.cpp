cbdf19ecb1ca5b1533915cfe77042902347431a4
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-17 13:30:24 +0530

    Swap Two Number Without Temp Variable
    Swap Two Number Without Temp Variable

---
+++ 14_swapNumWithoutTemp/14_swapNumWithoutTemp.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout << "ENter First Number: ";
+    cin >> num1;
+    cout << "Enter The Second Number: ";
+    cin >> num2;
+
+    cout << "Before Swap: " << num1 << num2 << endl;
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+
+    cout << "After Swap: " << num1 << num2;
+
+    return 0;
+}
