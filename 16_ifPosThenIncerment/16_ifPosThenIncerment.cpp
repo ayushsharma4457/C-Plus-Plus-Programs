7a5560b25afa0819c7723cd8435441022c376c9a
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-18 18:55:28 +0530

    Number Is Positive Then Increment
    Using If Statement.

---
+++ 16_ifPosThenIncerment/16_ifPosThenIncerment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter The Number: ";
+    cin >> num;
+
+    if (num > 0)
+    {
+        num++;
+        cout << num << endl;
+    }
+
+    return 0;
+}
